1bd04d9460b081aaeac3df4f8e8a1fea79bcb700
preiter <preiter@gwdg.de>
2021-07-29 12:50:11 +0200

    implement parallel transform

--- src/impl_transform.rs
+++ src/impl_transform.rs
@@ -2,6 +2,7 @@
 use crate::Base;
 use crate::FloatNum;
 use crate::Transform;
+use crate::TransformPar;
 use ndarray::prelude::*;
 
 /// Transform trait is implemented "per hand", can't be enum_dispatched
@@ -69,6 +70,75 @@ impl<A: FloatNum + std::ops::MulAssign> Transform for Base<A> {
         match self {
             Self::Chebyshev(ref mut b) => b.backward_inplace(input, output, axis),
             Self::CompositeChebyshev(ref mut b) => b.backward_inplace(input, output, axis),
+        }
+    }
+}
+
+/// Transform trait is implemented "per hand", can't be enum_dispatched
+/// because of associated types.
+impl<A: FloatNum + std::ops::MulAssign> TransformPar for Base<A> {
+    type Physical = A;
+    type Spectral = A;
+
+    fn forward_par<S, D>(
+        &mut self,
+        input: &mut ArrayBase<S, D>,
+        axis: usize,
+    ) -> Array<Self::Spectral, D>
+    where
+        S: ndarray::Data<Elem = Self::Physical>,
+        D: Dimension + ndarray::RemoveAxis,
+    {
+        match self {
+            Self::Chebyshev(ref mut b) => b.forward_par(input, axis),
+            Self::CompositeChebyshev(ref mut b) => b.forward_par(input, axis),
+        }
+    }
+
+    fn forward_inplace_par<S1, S2, D>(
+        &mut self,
+        input: &mut ArrayBase<S1, D>,
+        output: &mut ArrayBase<S2, D>,
+        axis: usize,
+    ) where
+        S1: ndarray::Data<Elem = Self::Physical>,
+        S2: ndarray::Data<Elem = Self::Spectral> + ndarray::DataMut,
+        D: Dimension + ndarray::RemoveAxis,
+    {
+        match self {
+            Self::Chebyshev(ref mut b) => b.forward_inplace_par(input, output, axis),
+            Self::CompositeChebyshev(ref mut b) => b.forward_inplace_par(input, output, axis),
+        }
+    }
+
+    fn backward_par<S, D>(
+        &mut self,
+        input: &mut ArrayBase<S, D>,
+        axis: usize,
+    ) -> Array<Self::Physical, D>
+    where
+        S: ndarray::Data<Elem = Self::Spectral>,
+        D: Dimension + ndarray::RemoveAxis,
+    {
+        match self {
+            Self::Chebyshev(ref mut b) => b.backward_par(input, axis),
+            Self::CompositeChebyshev(ref mut b) => b.backward_par(input, axis),
+        }
+    }
+
+    fn backward_inplace_par<S1, S2, D>(
+        &mut self,
+        input: &mut ArrayBase<S1, D>,
+        output: &mut ArrayBase<S2, D>,
+        axis: usize,
+    ) where
+        S1: ndarray::Data<Elem = Self::Spectral>,
+        S2: ndarray::Data<Elem = Self::Physical> + ndarray::DataMut,
+        D: Dimension + ndarray::RemoveAxis,
+    {
+        match self {
+            Self::Chebyshev(ref mut b) => b.backward_inplace_par(input, output, axis),
+            Self::CompositeChebyshev(ref mut b) => b.backward_inplace_par(input, output, axis),
         }
     }
 }
